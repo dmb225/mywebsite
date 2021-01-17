a0b0a28be1bbf3a0716a0ba6a560fa1418f9ec2f
dmb225 <daomalick225@gmail.com>
2021-01-17 15:05:43 +0100

    Add post sharing via email

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.views.generic import ListView
+from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -14,3 +16,24 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
     return render(request, 'post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{data['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{data['name']}\'s comments: {data['comments']}"
+            send_mail(subject, message, data['email'], [data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'post/share.html', {'post': post, 'form': form, 'sent': sent})
