4b2075c7c86a61ead131f88853f7e7477c1753f6
dmb225 <daomalick225@gmail.com>
2021-01-23 18:32:22 +0100

    Add comment system

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -15,19 +15,29 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request, 'post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment,
+                                                'comment_form': comment_form})
 
 
 def post_share(request, post_id):
-    # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
     if request.method == 'POST':
-        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # Form fields passed validation
             data = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{data['name']} recommends you read {post.title}"
