c0414bff9da088ef0322b5717bbf4ed904f084d4
dmb225 <daomalick225@gmail.com>
2021-02-05 14:41:53 +0100

    Add a search page

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.contrib.postgres.search import SearchVector
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
 
@@ -70,3 +71,16 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+    return render(request, 'post/search.html', {'form': form, 'query': query, 'results': results})
